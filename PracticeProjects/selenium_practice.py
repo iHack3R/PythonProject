151c4b77b14bae2f4efa707079caea56142012b9
Udit Kumar <udit247@live.in>
2022-08-24 21:38:18 +0530

    Python Practice v1

---
+++ PracticeProjects/selenium_practice.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.ChromiumEdge()
+driver.get("https://www.google.com/")
+driver.find_element(By.XPATH, "//input[@title='Search']").send_keys("Selenium")
+driver.find_element(By.XPATH, "//div[@class='CqAVzb lJ9FBc']//input[@name='btnK']").click()
+driver.implicitly_wait(5)
+driver.close()
